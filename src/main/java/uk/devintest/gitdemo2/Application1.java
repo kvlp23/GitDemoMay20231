0c6e7762fc2df5054b92a84dadd3ced97d1c8444
Pras <prasnuka@gmail.com>
2023-05-29 06:16:28 -0700

    commit

---
+++ src/main/java/uk/devintest/gitdemo2/Application1.java
@@ -0,0 +1,7 @@
+package uk.devintest.gitdemo2;
+
+public class Application1 {
+    public static void main(String[] args) {
+        System.out.println("Git is cool");
+    }
+}
---
+++ src/test/java/uk/devintest/git/test2/TestApp.java
@@ -0,0 +1,13 @@
+package uk.devintest.git.test2;
+
+
+
+public class TestApp {
+    public static void main(String[] args) {
+        System.out.println("Running Test 1");
+    }
+
+
+
+
+}
